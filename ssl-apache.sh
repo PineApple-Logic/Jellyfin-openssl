de423ae97448a4cc507ede745ab7dfc134c621f0
PineApple-Logic <63014709+PineApple-Logic@users.noreply.github.com>
2020-10-27 23:05:34 +0200

    Update ssl-apache.sh

--- ssl-apache.sh
+++ ssl-apache.sh
@@ -33,23 +33,23 @@ cd ~/Documents/openssl
 sudo chowm jellyfin:jellyfin jellyfin.pfx
 
 #Check
-echo.
+echo
 echo ------------------------------------------------------------------------
 echo 'Check for errors. if any error are found cancle (ctrl +c ) and report'
 echo ------------------------------------------------------------------------
-echo.
+echo
 echo press enter to continue
 read
 echo "0 0 * * *  root  certbot renew --quiet --no-self-upgrade --post-hook 'systemctl reload apache'" | sudo tee -a /etc/cron.d/renew_certbot
 
 #Reboot
 clear
-echo.
+echo
 echo '1.Add the jellyfin.pfx file to your SSL cert path in jellyfin'
 echo    'also finish all other requirmentsin jelyfin Network https'
-echo.
+echo
 echo '2.change port forwarding to 8096 to 80 and 8920 to 443'
-echo.
+echo
 echo '3.Reboot'
 echo press enter to continue
 read
