f465402de3435541f595cd78f87805c745833367
YodaQ <theyoda.q@gmail.com>
2020-12-19 23:07:00 +0200

    Changed how script handels saving of pfx file and fixed autorenew

--- ssl-nginx.sh
+++ ssl-nginx.sh
@@ -22,16 +22,14 @@ clear
 
 #Get Certs
 sudo certbot --nginx --agree-tos --redirect --hsts --staple-ocsp --email $Email -d $Domain --rsa-key-size 4096
-echo 'Enter cert.pem directory path'
+echo 'Please enter directory path where you want to save your certificate (Jellyfin must have access to this directory)'
 read path
 
 #Patch for Jellyfin
 clear
-sudo openssl pkcs12 -export -out jellyfin.pfx -inkey $path/privkey.pem -in $path/cert.pem -passout pass:
-mkdir ~/openssl
-sudo mv jellyfin.pfx ~/openssl
-cd ~/openssl
-sudo chown jellyfin:jellyfin jellyfin.pfx
+sudo openssl pkcs12 -export -out jellyfin.pfx -inkey /etc/letsencrypt/live/$Domain/privkey.pem -in /etc/letsencrypt/live/$Domain/cert.pem -passout pass:
+sudo cp jellyfin.pfx $path
+sudo chown jellyfin:jellyfin $path/jellyfin.pfx
 
 #Check
 echo
@@ -42,16 +40,18 @@ echo
 echo 'Press enter to continue'
 read
 echo "0 0 * * *  root  certbot renew --quiet --no-self-upgrade --post-hook 'systemctl reload nginx'" | sudo tee -a /etc/cron.d/renew_certbot
-
+echo "sudo openssl pkcs12 -export -out /etc/letsencrypt/live/$Domain/jellyfin.pfx -inkey /etc/letsencrypt/live/$Domain/privkey.pem -in /etc/letsencrypt/live/$Domain/cert.pem -passout pass:" | sudo tee -a /etc/cron.d/renew_certbot
+echo "cp /etc/letsencrypt/live/$Domain/jellyfin.pfx $path/jellyfin.pfx" | sudo tee -a /etc/cron.d/renew_certbot
+echo "sudo chown jellyfin:jellyfin $path/jellyfin.pfx" | sudo tee -a /etc/cron.d/renew_certbot
 #Reboot
 clear
 echo
 echo '1. Add the jellyfin.pfx file to your SSL cert path in jellyfin'
-echo   'also finish all other requirmentsin in jelyfin Network https'
+echo   'also finish all other requirments in jelyfin Network https'
 echo
 echo '2. Change port forwarding to 8096 to 80 and 8920 to 443'
 echo
-echo '3. Reboot'
+echo '3. Restart Jellyfin'
 echo
 echo 'Press enter to continue'
 read
