81328e3c43ba53a72db67c2751d568f6e00da8b3
PineApple-Logic <tristanrex2000@gmail.com>
2020-12-22 09:03:58 +0200

    Improvements

--- ssl-nginx.sh
+++ ssl-nginx.sh
@@ -23,7 +23,7 @@ clear
 #Get Certs
 sudo certbot --nginx --agree-tos --redirect --hsts --staple-ocsp --email $Email -d $Domain --rsa-key-size 4096
 clear
-if [ -e /etc/letsencrypt/live/$Domain/cert.pem ]
+sudo if [ -e /etc/letsencrypt/live/$Domain/cert.pem ]
   then
     echo
     echo "Certificate successfully created"
