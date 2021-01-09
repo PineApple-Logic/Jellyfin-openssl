0e9a2d4672731d8a9fc111bfa3be20e6ec2f1d12
PineApple-Logic <tristanrex2000@gmail.com>
2021-01-09 12:40:59 +0200

    The merg

--- openssl.sh
+++ openssl.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-#Install requirments
-sudo apt install certbot python3-certbot-apache
-clear
-
 #Pre-setup
+echo '1.apache or 2.nginx'
+read Number
+1 = apache
+2 = nginx
+echo $Number
+read
 echo 'Email address'
 read Email
 clear
 echo 'Domain'
 read Domain
+clear
+
+#Install requirments
+sudo apt install certbot python3-certbot-apache
 clear
 
 #Port Forward
@@ -44,15 +50,22 @@ clear
 sudo openssl pkcs12 -export -out jellyfin.pfx -inkey /etc/letsencrypt/live/$Domain/privkey.pem -in /etc/letsencrypt/live/$Domain/cert.pem -passout pass:
 sudo cp jellyfin.pfx $path
 sudo chown jellyfin:jellyfin $path/jellyfin.pfx
+if sudo [ -e $path/jellyfin.pfx ]
+  then
+    echo
+    echo "Jellyfin cert patch successfully"
+    echo
+  else
+    echo
+    echo "Failed to patch certificate for Jellyfin."
+   sleep 2s
+    echo
+    echo "Press enter to exit"
+    read
+    exit
+fi
 
-#Check
-echo
-echo --------------------------------------------------------------------------
-echo 'Check for errors. If any errors are found, cancel (ctrl + c ) and report'
-echo --------------------------------------------------------------------------
-echo
-echo 'Press enter to continue'
-read
+#Automated renewal
 echo "0 0 1 */2 *  root  certbot renew --quiet --no-self-upgrade --post-hook 'systemctl reload nginx'" | sudo tee -a /etc/cron.d/renew_certbot
 echo "1 0 1 */2 * sudo openssl pkcs12 -export -out /etc/letsencrypt/live/$Domain/jellyfin.pfx -inkey /etc/letsencrypt/live/$Domain/privkey.pem -in /etc/letsencrypt/live/$Domain/cert.pem -passout pass:" | sudo tee -a /etc/cron.d/renew_certbot
 echo "2 0 1 */2 * cp /etc/letsencrypt/live/$Domain/jellyfin.pfx $path/jellyfin.pfx" | sudo tee -a /etc/cron.d/renew_certbot
