6af642b7d13c64dfeab3c718cc08f4fcc33095d7
PineApple-Logic <tristanrex2000@gmail.com>
2021-01-09 13:23:05 +0200

    Upgrade

--- openssl.sh
+++ openssl.sh
@@ -1,33 +1,46 @@
 #!/bin/bash
 
 #Pre-setup
-echo '1.apache or 2.nginx'
-read Number
-1 = apache
-2 = nginx
-echo $Number
-read
-echo 'Email address'
-read Email
-clear
-echo 'Domain'
-read Domain
+echo '1.Apache or 2.Nginx'
+echo
+read -p 'Enter a Number:' num
+if [ $num = 1 ]
+  then
+    ser=apache
+  else
+   ser=nginx
+fi
+read -p 'Email address:' Email
+echo
+read -p 'Domain:' Domain
 clear
 
 #Install requirments
-sudo apt install certbot python3-certbot-apache
+declare -A osInfo;
+osInfo[/etc/redhat-release]=yum
+osInfo[/etc/arch-release]=pacman
+osInfo[/etc/gentoo-release]=emerge
+osInfo[/etc/SuSE-release]=zypp
+osInfo[/etc/debian_version]=apt
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        pman=${osInfo[$f]}
+    fi
+done
+sudo $pman install certbot python3-certbot-$ser
+read
 clear
 
 #Port Forward
 echo 'Port forward 80 to 80 and 443 to 443'
 echo '(Must be done to pass the cert challenge)'
 echo
-echo 'Press enter to continue'
-read
+read -p 'Press enter to continue'
 clear
 
 #Get Certs
-sudo certbot --apache --agree-tos --redirect --hsts --staple-ocsp --email $Email -d $Domain --rsa-key-size 4096
+sudo certbot --$ser --agree-tos --redirect --hsts --staple-ocsp --email $Email -d $Domain --rsa-key-size 4096
 clear
 if [ -e /etc/letsencrypt/live/$Domain/cert.pem ]
   then
@@ -37,18 +50,23 @@ if [ -e /etc/letsencrypt/live/$Domain/cert.pem ]
     sleep 2s
   else
     echo "Failed to create certificate."
-    echo "Recommende checking for any miss entries on your router before trying again."
+    echo "Troubleshoot list:"
+    echo "1. Check for miss entries on the router"
+    echo "2. Make sure $ser is running on port 80 (netstat -tulpn) else"
+    echo    "change it on the router to the port it is running on"
     sleep 2s
     read
     exit
 fi
-echo 'Please enter directory path where you want to save your certificate (Jellyfin must have access to this directory)'
+echo 'Please enter a directory path where you want to save your certificate'
+echo '(Jellyfin must have access to this directory)'
+echo
 read path
 
 #Patch for Jellyfin
 clear
 sudo openssl pkcs12 -export -out jellyfin.pfx -inkey /etc/letsencrypt/live/$Domain/privkey.pem -in /etc/letsencrypt/live/$Domain/cert.pem -passout pass:
-sudo cp jellyfin.pfx $path
+sudo mv jellyfin.pfx $path
 sudo chown jellyfin:jellyfin $path/jellyfin.pfx
 if sudo [ -e $path/jellyfin.pfx ]
   then
@@ -66,7 +84,7 @@ if sudo [ -e $path/jellyfin.pfx ]
 fi
 
 #Automated renewal
-echo "0 0 1 */2 *  root  certbot renew --quiet --no-self-upgrade --post-hook 'systemctl reload nginx'" | sudo tee -a /etc/cron.d/renew_certbot
+echo "0 0 1 */2 *  root  certbot renew --quiet --no-self-upgrade --post-hook 'systemctl reload $ser'" | sudo tee -a /etc/cron.d/renew_certbot
 echo "1 0 1 */2 * sudo openssl pkcs12 -export -out /etc/letsencrypt/live/$Domain/jellyfin.pfx -inkey /etc/letsencrypt/live/$Domain/privkey.pem -in /etc/letsencrypt/live/$Domain/cert.pem -passout pass:" | sudo tee -a /etc/cron.d/renew_certbot
 echo "2 0 1 */2 * cp /etc/letsencrypt/live/$Domain/jellyfin.pfx $path/jellyfin.pfx" | sudo tee -a /etc/cron.d/renew_certbot
 echo "3 0 1 */2 * sudo chown jellyfin:jellyfin $path/jellyfin.pfx" | sudo tee -a /etc/cron.d/renew_certbot
