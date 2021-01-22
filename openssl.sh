64766475946a8753363aebe428eadd6c4424ddc4
PineApple-Logic <tristanrex2000@gmail.com>
2021-01-22 14:35:11 +0200

    fully functioning changes

--- openssl.sh
+++ openssl.sh
@@ -2,17 +2,6 @@
 
 #Pre-setup
 clear
-
-#if [ -f '/usr/bin/nginx' ] fi
-#if [ -f '/usr/bin/nginx' ] fi
-
-
-#nginx=$(which nginx)
-#if [$nginx = '/usr/bin/nginx']
-#apache=$(which apache)
-#if [$nginx = '/usr/bin/nginx']
-
-
 echo '1.Apache or 2.Nginx'
 echo
 read -p 'Enter a Number:' num
@@ -37,29 +26,36 @@ read -p 'Domain:' domain
 clear
 
 #Installing requirments
-declare -A osinfo;
-osinfo['/etc/redhat-release']=yum
-osinfo['/etc/arch-release']=pacman
-osinfo['/etc/gentoo-release']=emerge
-osinfo['/etc/SuSE-release']=zypp
-osinfo['/etc/debian_version']=apt
+declare -A osInfo;
+osInfo[/etc/redhat-release]=yum
+osInfo[/etc/arch-release]=pacman
+osInfo[/etc/gentoo-release]=emerge
+osInfo[/etc/SuSE-release]=zypp
+osInfo[/etc/debian_version]=apt
+
 for f in ${!osInfo[@]}
 do
-    if [ -f /bin/${!osInfo[$f]} ]
-    then
-        pman=${osInfo[$f]}
-    else
-      clear
-      echo
-      echo 'Failed to identify your Package Manager'
-      echo 'Please Enter it below'
-      echo
-      read -p 'Package Manager:' pman
+    if [[ -f $f ]];then
+      pman=${osInfo[$f]}
+
     fi
 done
+command -v $pman >> pg.txt
+if [ -s pg.txt ]
+  then
+    rm -fr pg.txt
+    clear
+  else
+    rm -fr pg.txt
+    clear
+    echo Failed to identify your package Manager
+    echo Please enter it below
+    echo
+    read -p 'Package Manager:' pman
+fi
 echo 'Installing certbot'
 sudo $pman install certbot python3-certbot-$ser
-if [ -f "/usr/bin/certbot && -f /usr/bin/python3-certbot-$ser" ]
+if [ -f /usr/bin/certbot ]
   then
     clear
   else
@@ -70,48 +66,64 @@ if [ -f "/usr/bin/certbot && -f /usr/bin/python3-certbot-$ser" ]
     exit
 fi
 
+
+
 #Port Forward
-echo 'Port forward 80 to 80 and 443 to 443'
-echo '(Must be done to pass the cert challenge)'
-echo
-read -p 'Press enter to continue'
-clear
+sudo netstat -anp | grep $ser | awk 'NR==1{print $4}' | grep -Eo '[0-9]{1,4}' | tail -1  >> port.txt
+port=$(<port.txt)
+if [ -s port.txt ]
+  then
+    rm -fr port.txt
+    clear
+    echo "Port forward $port to 80 on your router."
+    echo 'Must be done to pass the cert test.'
+    echo
+    read -p 'Press Enter to continue'
+  else
+    rm -fr port.txt
+    clear
+    echo "Failed to fined which port number $ser is running on"
+    echo
+    echo "Make sure $ser is running"
+    echo "Try changing the port it is running on."
+    exit
+fi
 
 #Get Certs
-if [ $num =  1 ]
-  then
-    certbot certonly --apache --noninteractive --agree-tos --email $email -d $domain
-  else
-    sudo certbot --nginx --agree-tos --redirect --hsts --staple-ocsp --email $email -d $domain
+if [ $num = 1 ]
+then
+  sudo certonly --apache --noninteractive --agree-tos --email $email -d $domain
+else
+  sudo certbot --nginx --agree-tos --redirect --hsts --staple-ocsp --email $email -d $domain
 fi
-clear
-if [ -e "/etc/letsencrypt/live/$Domain/cert.pem" ]
+if sudo [ -e /etc/letsencrypt/live/$domain/cert.pem ]
   then
+    clear
     echo
     echo 'Certificate successfully created'
     echo
     sleep 1s
   else
+    echo
     echo 'Failed to create certificate.'
     echo
-    echo 'Troubleshoot list:'
-    echo '1. Check for miss entries on the router'
-    echo "2. Make sure $ser is running on port 80 (netstat -tulpn) else"
-    echo    'change the LAN port forwarding on the router to the port it is running on'
     exit
 fi
 echo 'Please enter a directory path where you want to save your certificate'
 echo '(Jellyfin must have access to this directory)'
 echo
+echo 'Example /home/%username$/openssl'
 read -p 'Directory:' path
 
 #Patch for Jellyfin
 clear
-sudo openssl pkcs12 -export -out jellyfin.pfx -inkey /etc/letsencrypt/live/$Domain/privkey.pem -in /etc/letsencrypt/live/$Domain/cert.pem -passout pass:
+sudo openssl pkcs12 -export -out jellyfin.pfx -inkey /etc/letsencrypt/live/$domain/privkey.pem -in /etc/letsencrypt/live/$domain/cert.pem -passout pass:
+mkdir $path
 sudo mv jellyfin.pfx $path
 sudo chown jellyfin:jellyfin $path/jellyfin.pfx
-if sudo [ -e "$path/jellyfin.pfx" ]
+if sudo [ -e $path/jellyfin.pfx ]
   then
+    clear
     echo
     echo 'Jellyfin cert patch successfully'
     echo
@@ -121,12 +133,12 @@ if sudo [ -e "$path/jellyfin.pfx" ]
     exit
 fi
 
-echo "0 0 * * 1 root certbot renew --quiet --no-self-upgrade --post-hook 'systemctl reload $ser'" | sudo tee -a /etc/cron.d/renew_certbot
-echo "0 0 * * 1 root openssl pkcs12 -export -out /etc/letsencrypt/live/$Domain/jellyfin.pfx -inkey /etc/letsencrypt/live/$Domain/privkey.pem -in /etc/letsencrypt/live/$Domain/cert.pem -passout pass:" | sudo tee -a /etc/cron.d/renew_certbot
-echo "0 0 * * 1 cp /etc/letsencrypt/live/$Domain/jellyfin.pfx $path/jellyfin.pfx" | sudo tee -a /etc/cron.d/renew_certbot
-echo "0 0 * * 1 root chown jellyfin:jellyfin $path/jellyfin.pfx" | sudo tee -a /etc/cron.d/renew_certbot
-echo "0 0 * * 1 root systemctl restart jellyfin.service" | sudo tee -a /etc/cron.d/renew_certbot
-
+#Automated renewal
+echo "0 0 0 */1 *  root  certbot renew --quiet --no-self-upgrade --post-hook 'systemctl reload $ser'" | sudo tee -a /etc/cron.d/renew_certbot
+echo "0 0 0 */1 * sudo openssl pkcs12 -export -out /etc/letsencrypt/live/$domain/jellyfin.pfx -inkey /etc/letsencrypt/live/$domain/privkey.pem -in /etc/letsencrypt/live/$Domain/cert.pem -passout pass:" | sudo tee -a /etc/cron.d/renew_certbot
+echo "0 0 0 */1 * cp /etc/letsencrypt/live/$domain/jellyfin.pfx $path/jellyfin.pfx" | sudo tee -a /etc/cron.d/renew_certbot
+echo "0 0 0 */1 * sudo chown jellyfin:jellyfin $path/jellyfin.pfx" | sudo tee -a /etc/cron.d/renew_certbot
+echo "0 0 0 */1 *  sudo systemctl restart jellyfin.service" | sudo tee -a /etc/cron.d/renew_certbot
 if [ -f  /etc/cron.d/renew_certbot ]
   then
     clear
@@ -138,7 +150,7 @@ if [ -f  /etc/cron.d/renew_certbot ]
     clear
 fi
 
-#Finish line
+#Reboot
 echo
 echo '1. Add the jellyfin.pfx file to your SSL cert path in jellyfin,'
 echo   'also finish all other requirments in jelyfin Network https'
