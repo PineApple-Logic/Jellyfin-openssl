394faf226002df6a0982784dab88a08359b33816
PineApple-Logic <tristanrex2000@gmail.com>
2021-01-22 10:24:23 +0200

    Cleanup. Added an exit command after the troubleshoot list

--- openssl.sh
+++ openssl.sh
@@ -87,6 +87,7 @@ if [ -e /etc/letsencrypt/live/$Domain/cert.pem ]
     echo '1. Check for miss entries on the router'
     echo "2. Make sure $ser is running on port 80 (netstat -tulpn) else"
     echo    'change the LAN port forwarding on the router to the port it is running on'
+    exit
 fi
 echo 'Please enter a directory path where you want to save your certificate'
 echo '(Jellyfin must have access to this directory)'
